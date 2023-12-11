0b3e121a2b51abbac5012214a7d2c37909aae051
ay30n591 <syhyeon591@gmail.com>
2023-12-11 15:02:34 +0900

    모바일 로그인 기능은 나중에 따로 보기위해 일단.. 제거

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -78,56 +78,6 @@ function Login({isLogin, setIsLogin}) {
             </PC>
 
 
-            <Mobile>
-                <h1>모바일</h1>
-                <div className='container' style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
-                        <div className='content'>
-                            <h1>BeatBuddy</h1>
-                            <div>
-                                <form  className='content__form'>
-                                {/* <form > */}
-
-                                    <div className='content__input'>
-                                    <label className='form-label' htmlFor='userid'>
-                                        <input onChange={onChange} type='text' id='userid' name='userid' placeholder='전화번호, 사용자 이름 또는 이메일'></input>
-                                        {/* <span>전화번호, 사용자 이름 또는 이메일 </span> */}
-                                    </label>
-                                    <label className="form-label" htmlFor="password">
-                                        <input onChange={onchange} type='password' id='password' name='password' placeholder='비밀번호' autoComplete='current-password' />
-                                        {/* <span>Password</span> */}
-                                    </label>
-                                </div>
-
-                                    <button type="sumbit"><b>Login</b></button>
-                                    <br/>
-
-                                </form>
-                            </div>
-
-                            <div class="content__or-text">
-                                <span></span>
-                                <span> or </span>
-                                <span></span>
-                            </div>
-                            <div class="content__forgot-buttons">
-                                <button>
-                                <GoogleOAuthProvider clientId={clientId}>
-                                    <GoogleLogin
-                                        onSuccess={(res) => {
-                                            console.log(res);
-                                        }}
-                                        onFailure={(err) => {
-                                            console.log(err);
-                                        }}
-                                    />
-                                </GoogleOAuthProvider>
-                            </button>
-                                <button>비밀번호를 잊으셨나요?</button>
-                            </div>
-
-                        </div>
-                    </div>
-            </Mobile>
         </div>
 
     );
