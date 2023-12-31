7d1cdb8b4da9c004cd175fe0b04b04f40d6efff1
eeeeeddy <marill94727@naver.com>
2023-12-31 17:48:31 +0900

    ### SignUp 수정 - 입력폼 수정 - 로그인 오류 수정   - db에 있는 아이디에 아무 비밀번호 입력해도 로그인되는 오류   - 구글 로그인 토큰 저장 코드 추가

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -8,13 +8,11 @@ function SignUp() {
     const [email, setEmail] = useState("");
     const [nickname, setNickname] = useState("");
     const [password, setPassword] = useState("");
-    const [passwordConfirm, setPasswordConfirm ] = useState("");
-    const [gender, setGender] = useState("");
-    const [birth, setBirth] = useState(""); 
+    const [passwordConfirm, setPasswordConfirm] = useState("");
     const [errorMessage, setErrorMessage] = useState([]);
-    const [passwordMatchError,setPasswordMatchError] = useState("");
+    const [passwordMatchError, setPasswordMatchError] = useState("");
     const navigate = useNavigate();
-
+    const token = window.localStorage.getItem("token");
 
     function onChange(event) {
         if (event.target.name === "nickname") {
@@ -23,12 +21,8 @@ function SignUp() {
             setEmail(event.target.value)
         } else if (event.target.name === "password") {
             setPassword(event.target.value)
-        } else if (event.target.name === "passwordConfirm"){
+        } else if (event.target.name === "passwordConfirm") {
             setPasswordConfirm(event.target.value)
-        } else if (event.target.name === "gender"){
-            setGender(event.target.value)
-        } else if (event.target.name === "birth") {
-            setBirth(event.target.value);
         }
     }
 
@@ -56,45 +50,37 @@ function SignUp() {
         }
         setErrorMessage(newErrorMessages);
 
+        // TODO: 구글 로그인 시 서버에서 이메일 정보 받아와야함 -> 그 후 수정하기
 
-        if(newErrorMessages.length === 0 ) {
+        if (newErrorMessages.length === 0) {
             window.localStorage.setItem('email', email);
-            window.localStorage.setItem('password', password);
-      
-            try { 
-                // const result = await axios.post("http://localhost:8080/api/v1/users/sign-up",{
-                    const result = await axios.post("https://94ed-121-190-220-40.ngrok-free.app/api/v1/users/sign-up",{
-                    email: email, 
+            window.localStorage.setItem('password', password); //왜저장하지?
+
+            try {
+                const result = await axios.post("https://94ed-121-190-220-40.ngrok-free.app/api/v1/users/sign-up", {
+                    email: email,
                     nickname: nickname,
                     password: password,
                     passwordConfirm: passwordConfirm,
-                    gender: gender,
-                    birth: birth
                 })
-                if(result.status === 200 ){
+                if (result.status === 200) {
                     console.log("가입 성공:", result);
 
                     navigate("/login")
                 }
-            } catch(error){
+            } catch (error) {
                 if (error.response && error.response.data) {
                     setErrorMessage([error.response.data]);
                     setNickname("")
                     setPassword("")
                     setEmail("")
                     setPasswordConfirm("")
-                    setBirth("")
                 } else {
                     console.log("가입 실패:", error);
                 }
             }
-            
-            //테스트
-            // navigate('/login');
         }
     }
-
-    
 
     return (
         <div>
@@ -102,129 +88,138 @@ function SignUp() {
                 <div className='container' >
                     <div className='content'>
                         <h1>BeatBuddy</h1>
+                        {token ? (
+                            <div>
+                                <form className='content__form' onSubmit={onSubmit}>
+                                    <div className='content__input'>
+                                        {errorMessage.length > 0 &&
+                                            (<div className="alert alert-danger" role="alert">
+                                                {errorMessage.map((message, index) => (<div key={index}>{message}</div>))}
+                                            </div>)
+                                        }
 
-                        <div>
-                            <form className='content__form' onSubmit={onSubmit}>
-                                <div className='content__input'>
-                                    {errorMessage.length > 0 &&
-                                        (<div className="alert alert-danger" role="alert">
-                                            {errorMessage.map((message, index) => (<div key={index}>{message}</div>))}
-                                        </div>)
-                                    }
-                                    <label className='form-label' htmlFor='email'>
-                                        <input onChange={onChange} value={email} type='email' id="email" name='email' placeholder='이메일'></input>
-                                    </label>
+                                        {/* TODO: 프로필 사진 업로드 추가하기 */}
 
-                                    <label className='form-label' htmlFor='username'>
-                                        <input onChange={onChange} type='text' id="nickname" name='nickname' placeholder='닉네임' ></input>
-                                    </label>
-
-                                    <label className='form-label' htmlFor='password'>
-                                        <input onChange={onChange} type="password" id="password" name="password" placeholder="비밀번호" autoComplete="new-password" ></input>
-                                    </label>
-                                    <label className='form-label' htmlFor='passwordConfirm'>
-                                        <input onChange={onChange} type="password" id="passwordConfirm" name="passwordConfirm" placeholder="비밀번호 확인" autoComplete="new-password" ></input>
-                                    </label>
-                                    {passwordMatchError && <div className="alert alert-danger">{passwordMatchError}</div>}
-
-                                    <div class="input-box" style={{ display: 'flex', gap: '10px' }}>
-                                        <label className='form-label' htmlFor='gender' style={{ flex: 1, minWidth: '130px' }}>
-                                            <select onChange={onChange} id="gender" name="gender">
-                                                <option value="">성별 선택</option>
-                                                <option value="male">남</option>
-                                                <option value="female">여</option>
-                                            </select>
-                                        </label>
-
-                                        <label className='form-label' htmlFor='birth' style={{ flex: 1, minWidth: '130px' }}>
-                                            <input onChange={onChange} id="birth" name="birth" required="" placeholder="생일" type="date"/>
+                                        <label className='form-label' htmlFor='username'>
+                                            <input onChange={onChange} type='text' id="nickname" name='nickname' placeholder='닉네임' ></input>
                                         </label>
                                     </div>
+                                    <button type='submit'><b>Sign Up</b></button>
+                                </form>
+                            </div>
+                        ) : (
+                            <div>
+                                <form className='content__form' onSubmit={onSubmit}>
+                                    <div className='content__input'>
+                                        {errorMessage.length > 0 &&
+                                            (<div className="alert alert-danger" role="alert">
+                                                {errorMessage.map((message, index) => (<div key={index}>{message}</div>))}
+                                            </div>)
+                                        }
+                                        <label className='form-label' htmlFor='email'>
+                                            <input onChange={onChange} value={email} type='email' id="email" name='email' placeholder='이메일'></input>
+                                        </label>
+
+                                        <label className='form-label' htmlFor='username'>
+                                            <input onChange={onChange} type='text' id="nickname" name='nickname' placeholder='닉네임' ></input>
+                                        </label>
+
+                                        <label className='form-label' htmlFor='password'>
+                                            <input onChange={onChange} type="password" id="password" name="password" placeholder="비밀번호" autoComplete="new-password" ></input>
+                                        </label>
+                                        <label className='form-label' htmlFor='passwordConfirm'>
+                                            <input onChange={onChange} type="password" id="passwordConfirm" name="passwordConfirm" placeholder="비밀번호 확인" autoComplete="new-password" ></input>
+                                        </label>
+                                        {passwordMatchError && <div className="alert alert-danger">{passwordMatchError}</div>}
+                                    </div>
+                                    <button type='submit'><b>Sign Up</b></button>
+
+                                    <br />
+                                </form>
+                                <div className="content__or-text">
+                                    <span></span>
+                                    <span></span>
                                 </div>
-                                <button type='submit'><b>Sign Up</b></button>
 
-                                <br/>
-                            </form>
-                            <div className="content__or-text">
-                                <span></span>
-                                <span></span>
+                                <div className="content__forgot-buttons">
+                                    <button> <Link to='/login'>
+                                        <span>Already have an acount ?</span> </Link>
+                                    </button>
+                                </div>
                             </div>
-                            
-                            <div className="content__forgot-buttons">
-                                <button> <Link to ='/login'>
-                                    <span>Already have an acount ?</span> </Link>
-                                </button>
-                            </div>
-                        </div>
-
+                        )}
                     </div>
                 </div>
-            
             </PC>
 
             <Mobile>
                 <div className='container' >
                     <div className='content'>
                         <h1>BeatBuddy</h1>
+                        {token ? (
+                            <div>
+                                <form className='content__form' onSubmit={onSubmit}>
+                                    <div className='content__input'>
+                                        {errorMessage.length > 0 &&
+                                            (<div className="alert alert-danger" role="alert">
+                                                {errorMessage.map((message, index) => (<div key={index}>{message}</div>))}
+                                            </div>)
+                                        }
 
-                        <div>
-                            <form className='content__form' onSubmit={onSubmit}>
-                                <div className='content__input'>
-                                    {errorMessage.length > 0 &&
-                                        (<div className="alert alert-danger" role="alert">
-                                            {errorMessage.map((message, index) => (<div key={index}>{message}</div>))}
-                                        </div>)
-                                    }
-                                    <label className='form-label' htmlFor='email'>
-                                        <input onChange={onChange} value={email} type='email' id="email" name='email' placeholder='이메일'></input>
-                                    </label>
+                                        {/* TODO: 프로필 사진 업로드 추가하기 */}
 
-                                    <label className='form-label' htmlFor='username'>
-                                        <input onChange={onChange} type='text' id="nickname" name='nickname' placeholder='닉네임' ></input>
-                                    </label>
-
-                                    <label className='form-label' htmlFor='password'>
-                                        <input onChange={onChange} type="password" id="password" name="password" placeholder="비밀번호" autoComplete="new-password" ></input>
-                                    </label>
-                                    <label className='form-label' htmlFor='passwordConfirm'>
-                                        <input onChange={onChange} type="password" id="passwordConfirm" name="passwordConfirm" placeholder="비밀번호 확인" autoComplete="new-password" ></input>
-                                    </label>
-                                    {passwordMatchError && <div className="alert alert-danger">{passwordMatchError}</div>}
-
-                                    <div class="input-box" style={{ display: 'flex', gap: '10px' }}>
-                                        <label className='form-label' htmlFor='gender' style={{ flex: 1, minWidth: '130px' }}>
-                                            <select onChange={onChange} id="gender" name="gender">
-                                                <option value="">성별 선택</option>
-                                                <option value="male">남</option>
-                                                <option value="female">여</option>
-                                            </select>
-                                        </label>
-
-                                        <label className='form-label' htmlFor='birth' style={{ flex: 1, minWidth: '130px' }}>
-                                            <input onChange={onChange} id="birth" name="birth" required="" placeholder="생일" type="date"/>
+                                        <label className='form-label' htmlFor='username'>
+                                            <input onChange={onChange} type='text' id="nickname" name='nickname' placeholder='닉네임' ></input>
                                         </label>
                                     </div>
+                                    <button type='submit'><b>Sign Up</b></button>
+                                </form>
+                            </div>
+                        ) : (
+                            <div>
+                                <form className='content__form' onSubmit={onSubmit}>
+                                    <div className='content__input'>
+                                        {errorMessage.length > 0 &&
+                                            (<div className="alert alert-danger" role="alert">
+                                                {errorMessage.map((message, index) => (<div key={index}>{message}</div>))}
+                                            </div>)
+                                        }
+                                        <label className='form-label' htmlFor='email'>
+                                            <input onChange={onChange} value={email} type='email' id="email" name='email' placeholder='이메일'></input>
+                                        </label>
+
+                                        <label className='form-label' htmlFor='username'>
+                                            <input onChange={onChange} type='text' id="nickname" name='nickname' placeholder='닉네임' ></input>
+                                        </label>
+
+                                        <label className='form-label' htmlFor='password'>
+                                            <input onChange={onChange} type="password" id="password" name="password" placeholder="비밀번호" autoComplete="new-password" ></input>
+                                        </label>
+                                        <label className='form-label' htmlFor='passwordConfirm'>
+                                            <input onChange={onChange} type="password" id="passwordConfirm" name="passwordConfirm" placeholder="비밀번호 확인" autoComplete="new-password" ></input>
+                                        </label>
+                                        {passwordMatchError && <div className="alert alert-danger">{passwordMatchError}</div>}
+                                    </div>
+                                    <button type='submit'><b>Sign Up</b></button>
+
+                                    <br />
+                                </form>
+                                <div className="content__or-text">
+                                    <span></span>
+                                    <span></span>
                                 </div>
-                                <button type='submit'><b>Sign Up</b></button>
 
-                                <br/>
-                            </form>
-                            <div className="content__or-text">
-                                <span></span>
-                                <span></span>
+                                <div className="content__forgot-buttons">
+                                    <button> <Link to='/login'>
+                                        <span>Already have an acount ?</span> </Link>
+                                    </button>
+                                </div>
                             </div>
-                            
-                            <div className="content__forgot-buttons">
-                                <button> <Link to ='/login'>
-                                    <span>Already have an acount ?</span> </Link>
-                                </button>
-                            </div>
-                        </div>
-
+                        )}
                     </div>
                 </div>
             </Mobile>
-        </div>
+        </div >
     );
 }
 
